3e9a467259ed79a2b3ab45c5ef6ab6bf29521dc6
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-11-22 08:59:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/weather-dashboard";
 import CityPage from "./pages/city-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
