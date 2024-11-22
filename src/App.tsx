c249ab6bc608cab87462e14eb62f88da7c2f947b
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-11-22 08:15:27 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,41 @@
-import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from "sonner";
 
-const App: React.FC = () => {
-  // Estado para a contagem
-  const [count, setCount] = useState<number>(0);
-
-  // Função para incrementar a contagem
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  // Função para decrementar a contagem
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <div style={styles.container}>
-      <h1 style={styles.header}>Contagem: {count}</h1>
-      <div style={styles.buttonContainer}>
-        <button style={styles.button} onClick={increment}>Incrementar</button>
-        <button style={styles.button} onClick={decrement}>Decrementar</button>
-      </div>
-    </div>
-  );
-};
-
-// Estilos em linha
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundColor: '#f0f0f0',
-    fontFamily: 'Arial, sans-serif',
-  },
-  header: {
-    color: '#333',
-  },
-  buttonContainer: {
-    marginTop: '20px',
-    display: 'flex',
-    gap: '10px',
-  },
-  button: {
-    padding: '10px 20px',
-    fontSize: '16px',
-    backgroundColor: '#4CAF50',
-    color: 'white',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime: 5 * 60 * 1000, //5 minutos
+      gcTime: 10 * 60 * 1000, //10 minutos
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
   }
-};
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="system">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+          <Toaster richColors/>
+        </ThemeProvider>
+      </BrowserRouter>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  );
+}
 
 export default App;
