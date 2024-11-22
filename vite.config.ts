95ebff09312104d79eaf612bc29c2f57af9989bc
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-11-22 09:15:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// Verifique se o ambiente de desenvolvimento está ativo
+const isDev = process.env.NODE_ENV === 'development';
+
+// Utilize o 'path' apenas em desenvolvimento
+let path = isDev ? import('path') : Promise.resolve(null);
+
+export default defineConfig(async () => {
+  const resolvedPath = await path;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolvedPath ? resolvedPath.default.resolve(__dirname, 'src') : 'src',
+      },
     },
-  },
+  };
 });
